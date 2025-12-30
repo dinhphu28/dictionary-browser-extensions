aac705d6cff705af716d5e7e31f8d5d39137d8ca
Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>
2025-12-30 18:18:24 +0700

    Remove old src 'chrome' & 'firefox'
    Signed-off-by: Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>

--- firefox/background.js
+++
@@ -1,9 +0,0 @@
-browser.runtime.onMessage.addListener(async (msg) => {
-  if (msg.type === "lookup") {
-    const res = await fetch(
-      `http://localhost:8080/lookup?q=${encodeURIComponent(msg.word)}`,
-    );
-    const data = await res.json();
-    return data;
-  }
-});
